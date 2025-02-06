71a2a10d95febed1cc4413d6ae7bc0a8ce410dcb
jdysya <1912377458@qq.com>
2025-02-06 22:09:21 +0800

    添加交易服务模块，包含订单、订单详情、订单物流相关的实体、DTO、Mapper和服务接口实现，更新配置文件，调整包结构

--- java/hm-api/src/main/java/com/hmall/api/dto/OrderDetailDTO.java
+++ java/hm-api/src/main/java/com/hmall/api/dto/OrderDetailDTO.java
@@ -1,4 +1,4 @@
-package com.hmall.item.domain.dto;
+package com.hmall.domain.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
---
+++ java/trade-service/src/main/java/com/hmall/trade/TradeApplication.java
@@ -0,0 +1,4 @@
+package com.hmall.trade;
+
+public class TradeApplication {
+}
